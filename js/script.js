19abad0fa5de8bbe5146f6041b717b37ee03ed63
Judyta Banas <banasjudyta@gmail.com>
2022-08-19 15:02:30 +0200

    remove active js

--- js/script.js
+++ js/script.js
@@ -2,33 +2,31 @@
 /*document.getElementById('test-button').addEventListener('click', function(){
   const links = document.querySelectorAll('.titles a');
   console.log('links:', links);
-});*/
+});
+*/
 
 
 const titleClickHandler = function(event){
   console.log('Link was clicked!');
-  console.log(event);
-
+  console.log(event)
 
   /* remove class 'active' from all article links  */
 
-const activeLinks = document.querySelectorAll('.titles a.active');
+  const activeLinks = document.querySelectorAll('.titles a.active');
 
 for(let activeLink of activeLinks){
   activeLink.classList.remove('active');
 }
 
-
   /* add class 'active' to the clicked link */
-
-  
 
   /* remove class 'active' from all articles */
 
-const activeArticles = document.querySelectorAll('.post');
+  const activeArticles = document.querySelectorAll('.post');
 
 for(let activeArticle of activeArticles){
-  activeLink.classList.remove('active');
+  activeArticle.classList.remove('active');
+}
 
   /* get 'href' attribute from the clicked link */
 
@@ -42,8 +40,3 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
-
-
-
-
-
