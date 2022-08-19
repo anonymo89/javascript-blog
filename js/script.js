903b90c075b1845592f3c7b0d4313ef0c2e0e95b
Judyta Banas <banasjudyta@gmail.com>
2022-08-19 13:49:02 +0200

    remove active js

--- js/script.js
+++ js/script.js
@@ -9,11 +9,26 @@ const titleClickHandler = function(event){
   console.log('Link was clicked!');
   console.log(event);
 
+
   /* remove class 'active' from all article links  */
+
+const activeLinks = document.querySelectorAll('.titles a.active');
+
+for(let activeLink of activeLinks){
+  activeLink.classList.remove('active');
+}
+
 
   /* add class 'active' to the clicked link */
 
+  
+
   /* remove class 'active' from all articles */
+
+const activeArticles = document.querySelectorAll('.post');
+
+for(let activeArticle of activeArticles){
+  activeLink.classList.remove('active');
 
   /* get 'href' attribute from the clicked link */
 
@@ -27,3 +42,8 @@ const links = document.querySelectorAll('.titles a');
 for(let link of links){
   link.addEventListener('click', titleClickHandler);
 }
+
+
+
+
+
